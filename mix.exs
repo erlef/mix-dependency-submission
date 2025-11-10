e3e07c533ea58301b54024fbf9663e1a167840a3
Jonatan Männchen <jonatan@maennchen.ch>
2025-11-10 17:57:48 +0100

    Release v1.2.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@
 defmodule MixDependencySubmission.MixProject do
   use Mix.Project
 
-  @version "1.2.1"
+  @version "1.2.2"
   @source_url "https://github.com/erlef/mix-dependency-submission"
   @description "Calculates dependencies for Mix and submits the list to the GitHub Dependency Submission API"
 
